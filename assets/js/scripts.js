87311dff7fc6fc1a890d0cfe841bc86becb1dab5
Jim Lai <jimlai1005@yahoo.com.tw>
2021-04-13 11:05:37 +0800

    Add Email Composition, Slots Game Entry, and Live Lobby Entry.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -42,6 +42,7 @@ jQuery(document).ready(function() {
 	$('#ow').hide();
 	$('#iframetag').hide();
 	$('#csid').hide();
+	$('#ecomp').hide();
 
 	$('#uaBtn').click(function() {
 	  $('#ua').show();
@@ -51,6 +52,8 @@ jQuery(document).ready(function() {
 	  $('#ow').hide();
 	  $('#iframetag').hide();
 	  $('#csid').hide();
+	  $('#ecomp').hide();
+
 	});
 	$('#dglBtn').click(function() {
 	  $('#ua').hide();
@@ -60,6 +63,8 @@ jQuery(document).ready(function() {
 	  $('#ow').hide();
 	  $('#iframetag').hide();
 	  $('#csid').hide();
+	  $('#ecomp').hide();
+
 	});
 	$('#ghBtn').click(function() {
 	  $('#ua').hide();
@@ -69,6 +74,8 @@ jQuery(document).ready(function() {
 	  $('#ow').hide();
 	  $('#iframetag').hide();
 	  $('#csid').hide();
+	  $('#ecomp').hide();
+
 	});
 	$('#gsBtn').click(function() {
 	  $('#ua').hide();
@@ -78,6 +85,8 @@ jQuery(document).ready(function() {
 	  $('#ow').hide();
 	  $('#iframetag').hide();
 	  $('#csid').hide();
+	  $('#ecomp').hide();
+
 	});
 	$('#owBtn').click(function() {
 	  $('#ua').hide();
@@ -87,6 +96,8 @@ jQuery(document).ready(function() {
 	  $('#ow').show();
 	  $('#iframetag').hide();
 	  $('#csid').hide();
+	  $('#ecomp').hide();
+
 	});
 	$('#iframetagBtn').click(function() {
 	  $('#ua').hide();
@@ -96,6 +107,8 @@ jQuery(document).ready(function() {
 	  $('#ow').hide();
 	  $('#iframetag').show();
 	  $('#csid').hide();
+	  $('#ecomp').hide();
+
 	});
 	$('#csidBtn').click(function() {
 	  $('#ua').hide();
@@ -105,67 +118,116 @@ jQuery(document).ready(function() {
 	  $('#ow').hide();
 	  $('#iframetag').hide();
 	  $('#csid').show();
+	  $('#ecomp').hide();
+
+	});
+
+	$('#ecompBtn').click(function() {
+	  $('#ua').hide();
+	  $('#dgl').hide();
+	  $('#gh').hide();
+	  $('#gs').hide();
+	  $('#ow').hide();
+	  $('#iframetag').hide();
+	  $('#csid').hide();
+	  $('#ecomp').show();
+
 	});
 	
+	$('#liveBtn').click(function() {
+	  $('.ua-category').val('blackjack');
+	  $('.ua-tid').val('g48qnr279zcxecmd');
+	});
+
+	$('#lobbyBtn').click(function() {
+	  $('.ua-category').val('blackjack');
+	  $('.ua-tid').val('');
+	});
+
+	$('#rtBtn').click(function() {
+	  $('.ua-category').val('10001nights');
+	  $('.ua-tid').val('10001nights00000');
+	});
+
+	$('#neBtn').click(function() {
+	  $('.ua-category').val('bloodsuckers');
+	  $('.ua-tid').val('bloodsuckers0000');
+	});
+
 	$('.ua form').submit(function(e) {
+		$('.ua-success-message').html('');
+		$('.ua-error-message').html('');
+
+		var uaGame = {};
+		if ($('.ua-tid').val().trim() == '') {
+			uaGame = {
+		        // "category": 'rng',
+		        "category": $('.ua-category').val().trim(),
+		        "interface": $('.ua-interface').val().trim()
+		    };
+		}
+		else {
+			uaGame = {
+		        // "category": 'rng',
+		        "category": $('.ua-category').val().trim(),
+		        "interface": $('.ua-interface').val().trim(),  
+		        "table": {
+		          "id": $('.ua-tid').val().trim()
+		        } 
+		    };
+		}
+
 		var formData = {
-	        	"uuid": $('.ua-uuid').val().trim(),
-			    "player": {
-			      "id": $('.ua-pid').val().trim(),
-			      "update": ($('.ua-update').val().trim() == 'true'),
-			      "firstName": $('.ua-firstName').val().trim(),
-			      "lastName": $('.ua-lastName').val().trim(),
-			      "nickname": $('.ua-nickname').val().trim(),
-			      "country": $('.ua-country').val().trim(),
-			      "language": $('.ua-language').val().trim(),
-			      "currency": $('.ua-currency').val().trim(),
-			      "session": {
-			        "id": $('.ua-sid').val().trim(),
-			        "ip": $('.ua-sip').val().trim()
-			      },
-			      "group": {
-			        "id": $('.ua-gid').val().trim(),
-			        "action": $('.ua-action').val().trim()
-			      }
-			    },
-			    "config": {
-			      "brand": {
-			        "id": $('.ua-bid').val().trim(),
-			        "skin": $('.ua-skin').val().trim()
-			      },
-			      "game": {
-			        "category": $('.ua-category').val().trim(),
-			        "interface": $('.ua-interface').val().trim(),  
-			        "table": {
-			          "id": $('.ua-tid').val().trim()
-			        }
-			      },
-			      "channel": {
-			        "wrapped": ($('.ua-wrapped').val().trim() == 'true'),
-			        "mobile": ($('.ua-mobile').val().trim() == 'true')
-			      },
-			      "urls": {
-			        "cashier": $('.ua-cashier').val().trim(),
-			        "responsibleGaming": $('.ua-responsibleGaming').val().trim(),
-			        "lobby": $('.ua-lobby').val().trim(),
-			        "sessionTimeout": $('.ua-sessionTimeout').val().trim(),
-			        "gameHistory": "http://www.RGam.ee",
-					"realityCheckURL": "http://www.RGam.ee",
-					"rngGoLiveURL": "http://www.RGam.ee",
-					"rngGoLiveURLMobile": "http://www.RGam.ee",
-					"rngLobbyButton": "http://www.RGam.ee",
-					"rngCloseButton": "http://www.RGam.ee",
-					"rngHomeButton": "http://www.RGam.ee",
-					"rngSessionTimeout": "http://www.RGam.ee",
-					"rngErrorHandling": "http://www.RGam.ee",
-					"sweSelfTest": "http://www.RGam.ee",
-					"sweGameLimits": "http://www.RGam.ee",
-					"sweSelfExclusion": "http://www.RGam.ee",
-					"freeGames": "http://www.RGam.ee"
-			      },
-			      "freeGames": ($('.ua-freeGames').val().trim() == 'true')
-			    }
-			};
+			"uuid": $('.ua-uuid').val().trim(),
+		    "player": {
+		      "id": $('.ua-pid').val().trim(),
+		      "update": ($('.ua-update').val().trim() == 'true'),
+		      "firstName": $('.ua-firstName').val().trim(),
+		      "lastName": $('.ua-lastName').val().trim(),
+		      "nickname": $('.ua-nickname').val().trim(),
+		      "country": $('.ua-country').val().trim(),
+		      "language": $('.ua-language').val().trim(),
+		      "currency": $('.ua-currency').val().trim(),
+		      "session": {
+		        "id": $('.ua-sid').val().trim(),
+		        "ip": $('.ua-sip').val().trim()
+		      },
+		      "group": {
+		        "id": $('.ua-gid').val().trim(),
+		        "action": $('.ua-action').val().trim()
+		      }
+		    },
+		    "config": {
+		      "brand": {
+		        "id": $('.ua-bid').val().trim(),
+		        "skin": $('.ua-skin').val().trim()
+		      },
+		      "game": uaGame,
+		      "channel": {
+		        "wrapped": ($('.ua-wrapped').val().trim() == 'true'),
+		        "mobile": ($('.ua-mobile').val().trim() == 'true')
+		      },
+		      "urls": {
+		        "cashier": $('.ua-cashier').val().trim(),
+		        "responsibleGaming": $('.ua-responsibleGaming').val().trim(),
+		        "lobby": $('.ua-lobby').val().trim(),
+		        "sessionTimeout": $('.ua-sessionTimeout').val().trim(),
+		        "gameHistory": "http://www.RGam.ee",
+				"realityCheckURL": "http://www.RGam.ee",
+				"rngGoLiveURL": "http://www.RGam.ee",
+				"rngGoLiveURLMobile": "http://www.RGam.ee",
+				"rngLobbyButton": "http://www.RGam.ee",
+				"rngCloseButton": "http://www.RGam.ee",
+				"rngHomeButton": "http://www.RGam.ee",
+				"rngSessionTimeout": "http://www.RGam.ee",
+				"rngErrorHandling": "http://www.RGam.ee",
+				"sweSelfTest": "http://www.RGam.ee",
+				"sweGameLimits": "http://www.RGam.ee",
+				"sweSelfExclusion": "http://www.RGam.ee"
+				},
+		      "freeGames": ($('.ua-freeGames').val().trim() == 'true')
+		}
+	};
 		e.preventDefault();
 	    var postdata = $('.ua form').serialize();
 	    formDataString = JSON.stringify(formData);
@@ -277,6 +339,7 @@ jQuery(document).ready(function() {
 	    $.ajax({
 	        type: 'GET',
 	        contentType: 'application/json',
+	        // url: 'https://' + $('.gs-licensee_hostname').val().trim() + '/api/streaming/game/v1/?transmissionId=76332d312d302d35342d31363134303634373132313735',
 	        url: 'https://' + $('.gs-licensee_hostname').val().trim() + '/api/streaming/game/v1/?startTime=' + timeNow,
 	        headers: {'Authorization': base64Encode},
 	        data: '',
@@ -344,13 +407,93 @@ jQuery(document).ready(function() {
 	    var postdata = $('.csid form').serialize();
 	    // formDataString = JSON.stringify(formData);
 		casino_id = ($('.csid-casino_name').val().toLowerCase().replace(' ','') + "000000000000000").substr(0,15)+ "1";
-		
-		$('.csid-success-message').html($('.csid-casino_name').val().toLowerCase().replace(' ',''));
+		casino_name = $('.csid-casino_name').val().toLowerCase().replace(' ','');
+		$('.csid-success-message').html(casino_name);
 		// $('.csid-success-message').html(JSON.stringify(json));
 		$('.csid-success-message').fadeIn();
 		$('.csid-error-message').html(casino_id);
 		// $('.csid-error-message').html('tables: ' + JSON.stringify(json.tables));
 		$('.csid-error-message').fadeIn();
+    	// $('.csid-iframeTag').html('<iframe frameBorder="0" width="1000" height="1345" scrolling="yes" src="https://' + $('.csid-licensee_hostname').val().trim() + '/api/streaming/game/v1/?startTime=' + timeNow +'"></iframe>');
+	});
+
+// ECOMP API	
+	$('.ecomp form').submit(function(e) {
+		e.preventDefault();
+	    var postdata = $('.ecomp form').serialize();
+	    // formDataString = JSON.stringify(formData);
+		casino_id = ($('.ecomp-casino_name').val().toLowerCase().replace(' ','') + "000000000000000").substr(0,15)+ "1";
+		casino_name = $('.ecomp-casino_name').val().toLowerCase().replace(' ','');
+		context = '<div style="text-align:left">\
+			<br>Dear Licensee,\
+			<br>\
+			<br>Hope to find you well. \
+			<br>\
+			<br>The information below is going to help you to build up your services, pass the tests and launch your games. Now, please allow me to explain the process as below:\
+			<br>\
+			<br>- Process:\
+			<br>\
+			<br>1. API Test: There is no user interface required in this phase. Only http request and response are needed. The purpose is to verify the wallet service works as expected. You will need the Staging Credentials, Online Documents and Table List as below to do it and please whitelist the following IPs: 87.246.163.0/24, 35.157.123.250, and 72.2.49.45.\
+			<br>\
+			<br>2. QA Test: In this phase, we focus on the user experience in the testing website. The test starts on players to login your testing website, launch Evolution games, place bets and etc. \
+			<br>\
+			<br>3. Config Production: Once above tests are passed, we will setup the production config for you. Then, after a quick check in production, you will be ready to go!\
+			<br>\
+			<br>- Staging Credentials:\
+			<br>\
+			<br>    Casino id: ' + casino_id + '\
+			<br>\
+			<br>    {\
+			<br>\
+			<br>    "host" : "' + casino_name + '.uat1.evo-test.com",\
+			<br>\
+			<br>    "ua2Token" : "test123",\
+			<br>\
+			<br>    "gameHistoryApiToken" : "test123",\
+			<br>\
+			<br>    "externalLobbyApiToken" : "test123",\
+			<br>\
+			<br>    "ua2AuthenticationUrl" : "https://' + casino_name + '.uat1.evo-test.com/ua/v1/' + casino_id + '/test123",\
+			<br>\
+			<br>    "owAuthToken" : "8kZoqVAreAR0ZajzG"\
+			<br>\
+			<br>    }\
+			<br>\
+			<br>- Online Documents:\
+			<br>\
+			<br>    1. User Authentication API (to launch games)\
+			<br>\
+			<br>    https://' + casino_name + '.uat1.evo-test.com/api/userauthentication/docs/v2/protocol.html\
+			<br>\
+			<br>    2. Game Streaming API (for instant game round records)\
+			<br>\
+			<br>    https://' + casino_name + '.uat1.evo-test.com/api/streaming/game/docs/index.html\
+			<br>\
+			<br>    3. Game History API\
+			<br>\
+			<br>    https://' + casino_name + '.uat1.evo-test.com/api/gamehistory/docs/index.html\
+			<br>\
+			<br>    4. Direct Game Launch API (for getting status of current lobby)\
+			<br>\
+			<br>    https://' + casino_name + '.uat1.evo-test.com/api/lobby/docs/index.html\
+			<br>\
+			<br>    5. One wallet API\
+			<br>\
+			<br>    https://' + casino_name + '.uat1.evo-test.com/api/evo-std-rest/docs/index.html\
+			<br>\
+			<br>- Table List:\
+			<br>\
+			<br>    Please find the table list information for your reference in the attachment and also note the tables in production may differ from staging as it depends on your contract with our commercial team.\
+			<br>\
+			<br>Anything, please feel free to let me know. Thank you.\
+			<br>\
+			</div>'
+		$('.ecomp-success-message').html($('.ecomp-casino_name').val().toLowerCase().replace(' ',''));
+		$('.ecomp-success-message').html(context);
+		$('.ecomp-success-message').fadeIn();
+		$('.ecomp-error-message').html();
+		// $('.csid-error-message').html('tables: ' + JSON.stringify(json.tables));
+		$('.ecomp-error-message').fadeIn();
     	// $('.csid-iframeTag').html('<iframe frameBorder="0" width="1000" height="1345" scrolling="yes" src="https://' + $('.csid-licensee_hostname').val().trim() + '/api/streaming/game/v1/?startTime=' + timeNow +'"></iframe>');
 	});
 
